3a0aaadcd4cf3b2214eb0b40d606f2d0ee6bd0f5
Leonard Marshall Afzal <loma256@gmail.com>
2022-02-02 16:26:06 +0000

    Extend models.py with Club and Movie
    - Create Club model
    - Create Movie model
    - Extend User model with joined clubs function

--- api/models.py
+++ api/models.py
@@ -1,5 +1,9 @@
+from enum import unique
+from lib2to3.pgen2.pgen import generate_grammar
+from re import T
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.forms import CharField
 
 """Creation of user model"""
 
@@ -34,7 +38,62 @@ class User(AbstractUser):
         unique = False
     )
 
+    def get_user_clubs(self):
+        clubs = Club.objects.all().filter(
+            club_members__username=self.username
+        )
+        return clubs
+
     class Meta:
         """Model options."""
         ordering = ['last_name', 'first_name']
+
+class Club(models.Model):
+    
+    club_name = models.CharField(
+        max_length=50, 
+        blank=False, 
+        unique=False
+    )
+
+    mission_statement = models.CharField(
+        max_length=500, 
+        blank=True, 
+        unique=False
+    )
+
+    club_members = models.ManyToManyField(
+        User,through='Membership'
+    )
+
+    def get_club_role(self, user):
+        return Membership.objects.get(club=self, user=user).role
+
+class Movie(models.Model):
+
+    movie_name = models.CharField(
+        max_length=100,
+        blank=False,
+        unique=True
+    )    
+    
+    release_date =models.DateField(auto_now=False, auto_now_add=False, **options)
+    
+    # genre = models.Choices()  # Use dictionary to pass through all genres in the dataset
+    # cast
+    director = CharField(
+        
+        max_length=50,
+        blank=False,
+        unique=False,
+    )
+    # like  # Relationship with usermodel
+    # dislike
+
+    class Meta:
+        ordering = ['movie_name']
+
+
+
+
 
