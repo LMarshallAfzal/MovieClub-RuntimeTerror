e448dec0896a3dabeaced384be8ef3be482a7d29
Binh <13987638+koblihh@users.noreply.github.com>
2022-02-03 12:01:54 +0000

    Fix up inconsistent code formatting
    - Remove unnecessary imports

--- api/models.py
+++ api/models.py
@@ -1,40 +1,22 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-"""Creation of user model"""
 
 class User(AbstractUser):
-
-    username = models.CharField(
-        max_length = 15,
-        unique = True,
-        blank = False  
-    )
-
-    first_name = models.CharField(
-        max_length = 30,
-        blank = False,
-        unique = False
-    )
-
-    last_name = models.CharField(
-        max_length = 30,
-        blank = False,
-        unique = False,
-    )
-
-    email = models.EmailField(
-        unique = True,
-        blank = False
-    )
-
-    bio = models.CharField(
-        max_length = 520,
-        blank = True,
-        unique = False
-    )
+    username = models.CharField(max_length=15, unique=True, blank=False)
+    first_name = models.CharField(max_length=30, blank=False, unique=False)
+    last_name = models.CharField(max_length=30, blank=False, unique=False)
+    email = models.EmailField(unique=True, blank=False)
+    bio = models.CharField(max_length=520, blank=True, unique=False)
 
     class Meta:
-        """Model options."""
-        ordering = ['last_name', 'first_name']
+        ordering = ["last_name", "first_name"]
 
+    def clubs(self):
+        clubs = Club.objects.all().filter(club_members__username=self.username)
+        return clubs
+
+
+class Club(models.Model):
+    club_name = models.CharField(max_length=50, blank=False, unique=False)
+    mission_statement = models.CharField(max_length=500, blank=True, unique=False)
