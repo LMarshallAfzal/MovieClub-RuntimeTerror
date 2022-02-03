8fb70b58dab4a372af0304410362281962391961
William Cooper <cooperwilliam92@gmail.com>
2022-02-03 14:44:16 +0000

    Implement genres tests

--- api/test/models/test_movie_model.py
+++ api/test/models/test_movie_model.py
@@ -43,6 +43,21 @@ class MovieModelTestCase(APITestCase):
         self.movie.release_date = self.second_movie.release_date
         self._assert_movie_is_valid()
     
+    def test_genres_need_not_be_unique(self):
+        self.movie.genres = self.second_movie.genres
+        self._assert_movie_is_valid()
+
+    def test_genres_must_not_be_blank(self):
+        self.movie.genres = ''
+        self._assert_movie_is_invalid()
+
+    def test_genres_contain_100_characters_at_most(self):
+        self.movie.genres = 'x' * 100
+        self._assert_movie_is_valid()
+
+    def test_genres_must_not_contain_more_than_100_characters(self):
+        self.movie.genres = 'x' * 101
+        self._assert_movie_is_invalid()
 
     
     
