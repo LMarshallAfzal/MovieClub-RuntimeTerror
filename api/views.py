46ba7d86da9f88bf976862ffa0d8b1214213fdcb
josefsousa09 <jfrancisco.mail@gmail.com>
2022-02-04 17:45:47 +0000

    Add feed to getRoute and create a URL path.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import *
+from .models import *
+from api import serializers
 
-# Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/feed/',
+            'method': 'GET',
+            'description': 'Returns an array of notes'
+        }
+
+    ]
+
+
+
