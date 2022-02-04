edb2fb0bd8c7b8a295d1ad986304c048be223f24
Leonard Marshall Afzal <loma256@gmail.com>
2022-02-04 21:45:28 +0000

    Add signUp view
    - Add sign up view to views.py
    - Implement sign up view into 'getRoutes'
    - Add sign up view path to api/urls.py

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import *
 from .models import *
 from api import serializers
@@ -8,12 +9,28 @@ from api import serializers
 def getRoutes(request):
     routes = [
         {
-            'Endpoint': '/feed/',
-            'method': 'GET',
-            'description': 'Returns an array of notes'
+            'Endpoint': '/signup/',
+            'method': 'POST',
+            'username': {'username,' ""},
+            'first_name': {'first_name': ""},
+            'last_name': {'last_name': ""},
+            'email': {'email': ""},
+            'bio': {'bio': ""},
+            'description': 'Creates a user with data sent into POST request'
         }
-
     ]
+    return Response(routes)
 
-
+@api_view(['POST'])
+def signUp(request):
+    serializer = SignUpSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        user = serializer.save()
+        data['response'] = "successfully signed up"
+        data['email'] = user.email
+        data['username'] = user.username
+    else:
+        data = serializer.errors
+    return Response(data)
 
