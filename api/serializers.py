7ab9c8ee5b1eb59adeb29156e656d810017f8f85
Leonard Marshall Afzal <loma256@gmail.com>
2022-02-04 21:08:05 +0000

    Add a sign up serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,33 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from .models import Membership, Movie, User, Club
+from .models import *
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class SignUpSerializer(ModelSerializer):
+    password_confirmation = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['__all__', 'password', 'password_confirmation']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = User(
+            username = self.validated_data['username'],
+        )
+        password = self.validated_data['password']
+        password_confirmation = self.validated_data['password_confirmation']
+
+        if password != password_confirmation:
+            raise serializers.ValidationError({'password': 'Password fields must match'})
+        user.set_password(password)
+        user.save()
 
 class ClubSerializer(ModelSerializer):
     class Meta:
