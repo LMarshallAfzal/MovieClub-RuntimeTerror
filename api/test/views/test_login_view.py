af916c69aa7164fdf4c4cbcd6390ade8be19a9ca
josefsousa09 <jfrancisco.mail@gmail.com>
2022-02-05 04:43:57 +0000

    Add testcases to login view

---
+++ api/test/views/test_login_view.py
@@ -0,0 +1,45 @@
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from api.models import User
+
+class LoginViewTestcase(APITestCase):
+
+    fixtures = [
+        'api/test/fixtures/default_user.json',
+        'api/test/fixtures/other_users.json',
+    ]
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.get(username='johndoe')
+        self.url = reverse('login')
+
+    def test_login_url(self):
+        self.assertEqual(self.url, '/login/')
+
+    def test_user_logged_in_successfully(self):
+        details = {'username' : self.user.username, 'password':'Password123'}
+        response = self.client.login(
+            username = details['username'],password = details['password']
+        )
+        self.assertTrue(response)
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #status code cant be 200
+    def test_user_logged_in_unsuccessfully(self):
+        details = {'username' : "wrongusername", 'password':'Password123'}
+        response = self.client.login(
+            username = details['username'],password = details['password']
+        )
+        self.assertFalse(response)
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+        
+
+
+
+    
