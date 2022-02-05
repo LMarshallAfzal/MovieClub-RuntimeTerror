cb4223a06e38572cb7beb2cdffcbc061c6af6776
josefsousa09 <jfrancisco.mail@gmail.com>
2022-02-05 03:34:55 +0000

    Add login serializer and login view.

--- api/views.py
+++ api/views.py
@@ -2,24 +2,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
-from api import serializers
 
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
-            'Endpoint': 'login/id',
-            'username': None,
+            'Endpoint': '/login/',
+            'method': 'POST',
+            'username': {'username': ""},
+            'password': {'password': ""},
             'description': 'Return login details'
         }
     ]
     return Response(routes)
 
-@api_view(['GET'])
-def login(request, pk):
-    user = User.objects.get(id=pk)
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
-
-
-
+@api_view(['POST'])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        data['response'] = 'User login successful'
+    else:
+        data['response'] = 'You have entered an invalid username or password'
+    return Response(data)
