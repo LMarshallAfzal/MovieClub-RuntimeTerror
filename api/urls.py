d5a3f6bbc56d55fcc3934bfc5a670675bc572e69
josefsousa09 <jfrancisco.mail@gmail.com>
2022-02-06 18:52:50 +0000

    Update url.py with correct name.

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('sign_up/', views.login, name = 'sign_up'),
-    path('log_in/', views.login, name = 'login'),
+    path('sign_up/', views.signUp, name = 'sign_up'),
+    path('log_in/', views.login, name = 'log_in'),
 ]
