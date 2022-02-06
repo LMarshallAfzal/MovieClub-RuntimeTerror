83d372ab64d30d3a55b383f022928fd0a9b7c7f6
josefsousa09 <jfrancisco.mail@gmail.com>
2022-02-06 18:09:43 +0000

    Add unique username testcase.

--- api/test/views/test_signup_view.py
+++ api/test/views/test_signup_view.py
@@ -1,13 +1,18 @@
 from api.models import User
 from rest_framework.test import APITestCase
-
+from django.urls import reverse
+from rest_framework import status
 
 class SignUpViewTestCase(APITestCase):
-    def test_post_to_sign_up_endpoint_with_valid_data_creates_new_user(self):
-        before = User.objects.count()
-        url = "/sign_up/"
-        input = {
-            "username": "@johndoe",
+    
+    fixtures = [
+        'api/test/fixtures/other_users.json',
+    ]
+
+    def setUp(self):
+        self.url = reverse('sign_up')
+        self.form_input = {
+            "username": "johndoe",
             "first_name": "John",
             "last_name": "Doe",
             "email": "johndoe@example.org",
@@ -16,25 +21,20 @@ class SignUpViewTestCase(APITestCase):
             "password": "complexpassword",
             "password_confirmation": "complexpassword",
         }
-        response = self.client.post(url, input)
+        self.second_user = User.objects.get(username='janedoe')
+
+    def test_post_to_sign_up_endpoint_with_valid_data_creates_new_user(self):
+        before = User.objects.count()
+        response = self.client.post(self.url, self.form_input)
         after = User.objects.count()
         self.assertEqual(after, before + 1)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_post_to_sign_up_endpoint_with_invalid_data_does_not_create_new_user(self):
+    def test_post_to_sign_up_endpoint_with_not_unique_username_does_not_create_new_user(self):
         before = User.objects.count()
-        url = "/sign_up/"
-        input = {
-            "username": "",
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "johndoe@example.org",
-            "bio": "The quick brown fox jumps over the lazy dog.",
-            "preferences": "Action, Drama, Horror, Comedy, Science fiction",
-            "password": "Password123",
-            "password_confirmation": "Foobar69",
-        }
-        response = self.client.post(url, input)
+        input = self.form_input
+        input['username'] = self.second_user.username
+        response = self.client.post(self.url, input)
         after = User.objects.count()
         self.assertEqual(after, before)
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
