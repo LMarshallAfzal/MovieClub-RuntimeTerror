a490a76f412bf0085f6660202c7222e7fa900b37
Binh <13987638+koblihh@users.noreply.github.com>
2022-02-07 14:59:54 +0000

    Configure new log out endpoint

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.getRoutes),
     path('sign_up/', views.signUp, name = 'sign_up'),
     path('log_in/', views.login, name = 'log_in'),
+    path("log_out/", views.log_out, name="log_out"),
 ]
