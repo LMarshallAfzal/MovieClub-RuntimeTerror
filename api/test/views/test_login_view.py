e9a5c578699eac5fd17c7be5458a42ccdf763e30
Zsolt Balla <balla.zsolt.krisztian@gmail.com>
2022-02-07 15:21:01 +0000

    Add further tests for login and signup
    - Also correct tests in test_login_view

--- api/test/views/test_login_view.py
+++ api/test/views/test_login_view.py
@@ -1,4 +1,39 @@
-from rest_framework import APIRequestFactory
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from api.models import User
 
-# Using standard request factory API to create form request
-factory = APIRequestFactory
+class LoginViewTestcase(APITestCase):
+
+    fixtures = [
+        'api/test/fixtures/default_user.json',
+        'api/test/fixtures/other_users.json',
+    ]
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.get(username='johndoe')
+        self.url = reverse('log_in')
+
+    def test_login_url(self):
+        self.assertEqual(self.url, '/log_in/')
+
+    def test_user_logged_in_successfully(self):
+        details = {'username' : self.user.username, 'password':'Pa$$w0rd567'}
+        response = self.client.post(self.url, details)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_wrong_username_returns_400(self):
+        details = {'username' : "wrongusername", 'password':'Pa$$w0rd567'}
+        response = self.client.post(self.url,details)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_username_password_may_not_be_blank(self):
+        details = {'username' : "", 'password':""}
+        response = self.client.post(self.url, details)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_wrong_password_returns_400(self):
+        details = {'username' : self.user.username, 'password':'wrongpassword'}
+        response = self.client.post(self.url, details)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
