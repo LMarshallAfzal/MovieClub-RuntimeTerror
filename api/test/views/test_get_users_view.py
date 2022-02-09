7247578f56354cb14e8cf2d6f6e5530c654e8b97
Leonard Marshall Afzal <loma256@gmail.com>
2022-02-09 18:51:10 +0000

    Extend 'test_get_users_views' with test checking response data is right

--- api/test/views/test_get_users_view.py
+++ api/test/views/test_get_users_view.py
@@ -3,6 +3,8 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 
+from api.serializers import UserSerializer
+
 
 
 class GetUsersViewTestCase(APITestCase):
@@ -19,10 +21,13 @@ class GetUsersViewTestCase(APITestCase):
     def test_get_users_url(self):
         self.assertEqual(self.url, '/users/')    
 
-    def test_get_from_get_users_endpoint_returns_all_users(self):
+    def test_gets_all_users(self):
+        self.client.force_login(self.user)
         response = self.client.get(self.url)
-        user_count = User.objects.count()
-        self.assertEqual(user_count, 2)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_logged_in_user_can_view_users(self):
         self.client.force_login(self.user)
