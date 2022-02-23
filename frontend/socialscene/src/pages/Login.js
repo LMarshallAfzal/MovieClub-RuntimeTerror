406a54a96fe62b31f3b73f69a85fe5b060e4b4f9
noahcheeseman <noah.cheeseman@kcl.ac.uk>
2022-02-23 20:02:25 +0000

    finish Login.js alignment

--- frontend/socialscene/src/pages/Login.js
+++ frontend/socialscene/src/pages/Login.js
@@ -2,24 +2,20 @@ import React from "react";
 import "../styling/pages/Login.css";
 // import {Stack} from "react-bootstrap";
 import HeadingCircle from "../components/HeadingCircle";
-import {Stack, TextField} from "@mui/material";
+import {Grid, Stack, TextField} from "@mui/material";
 import FormButton from "../components/FormButton";
 import RoundButtonForward from "../components/RoundButtonForward";
 
 
 function login() {
     return (
-        <login className={"login"}>
-
-            <login className={"login-left"}>
-
+        <Grid className={"login-grid"} container spacing={2}>
+            <Grid className={"login-grid-left"} item xs={6}>
                 <HeadingCircle title={"log in"}/>
+            </Grid>
 
-            </login>
-
-            <login className={"login-right"}>
-
-                <Stack spacing={3}>
+            <Grid className={"login-grid-right"} item xs={6}>
+                <Stack className={"form-stack"} spacing={3}>
                     <TextField
                         id={"outlined-basic"}
                         label={"username"}
@@ -39,19 +35,53 @@ function login() {
                     <FormButton
                         text={"log in"}
                     />
-                    {/*<div className={"dual-button"}>*/}
-                    {/*    /!*<div className={"dual-button-left"}>*!/*/}
-                    {/*        <FormButton*/}
-                    {/*            text={"forgot password"}*/}
-                    {/*        />*/}
-                    {/*    /!*</div>*!/*/}
-                    {/*    /!*<div className={"dual-button-right"}>*!/*/}
-                    {/*        <RoundButtonForward text={"login"}/>*/}
-                    {/*    /!*</div>*!/*/}
-                    {/*</div>*/}
                 </Stack>
-            </login>
-        </login>
+            </Grid>
+        </Grid>
+
+        // <login className={"login"}>
+        //
+        //     <login className={"login-left"}>
+        //
+        //         <HeadingCircle title={"log in"}/>
+        //
+        //     </login>
+        //
+        //     <login className={"login-right"}>
+        //
+        //         <Stack spacing={3}>
+        //             <TextField
+        //                 id={"outlined-basic"}
+        //                 label={"username"}
+        //                 variant={"outlined"}
+        //             />
+        //
+        //             <TextField
+        //                 id={"outlined-basic"}
+        //                 label={"password"}
+        //                 variant={"outlined"}
+        //             />
+        //
+        //             <FormButton
+        //                 text={"forgot password"}
+        //             />
+        //
+        //             <FormButton
+        //                 text={"log in"}
+        //             />
+        //             {/*<div className={"dual-button"}>*/}
+        //             {/*    /!*<div className={"dual-button-left"}>*!/*/}
+        //             {/*        <FormButton*/}
+        //             {/*            text={"forgot password"}*/}
+        //             {/*        />*/}
+        //             {/*    /!*</div>*!/*/}
+        //             {/*    /!*<div className={"dual-button-right"}>*!/*/}
+        //             {/*        <RoundButtonForward text={"login"}/>*/}
+        //             {/*    /!*</div>*!/*/}
+        //             {/*</div>*/}
+        //         </Stack>
+        //     </login>
+        // </login>
     );
 }
 
