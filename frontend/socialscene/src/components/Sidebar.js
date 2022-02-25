1512a3ca6bfa51502836f4348815a7207acfafd3
noahcheeseman <noah.cheeseman@kcl.ac.uk>
2022-02-25 20:35:30 +0000

    small refinements

--- frontend/socialscene/src/components/Sidebar.js
+++ frontend/socialscene/src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SidebarData } from './SidebarData'
 import "../styling/components/Sidebar.css";
-import {Router, Route,Link, useLocation} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 function sidebar() {
   return (
