53952549e619606dfb676e53836a723c12006920
noahcheeseman <noah.cheeseman@kcl.ac.uk>
2022-03-02 12:17:54 +0000

    API V.2 test

---
+++ frontend/session_auth/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import CheckAuthenticatedView, SignupView, LoginView, LogoutView, GetCSRFToken, DeleteAccountView
+
+urlpatterns = [
+    path('authenticated', CheckAuthenticatedView.as_view()),
+    path('register', SignupView.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
+    path('csrf_cookie', GetCSRFToken.as_view()),
+    path('delete', DeleteAccountView.as_view())
+]
