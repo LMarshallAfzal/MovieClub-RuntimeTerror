e28d977b089943ec7092a5b7e2518a5d631f0f78
Leonard Marshall Afzal <loma256@gmail.com>
2022-03-05 13:35:44 +0000

    Update movies.js with cards to for movies to watch

--- frontend/socialscene/src/pages/Movies.js
+++ frontend/socialscene/src/pages/Movies.js
@@ -1,11 +1,75 @@
 import React from "react";
+import {Box, Grid, Stack, TextField, Button} from "@mui/material";
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import avengerImage from "../styling/avengers.jpg";
+import djangoImage from "../styling/django.jpeg";
+import jackImage from "../styling/jack-giant.jpg";
 
-function movies() {
+
+function Movies() {
     return (
-        <>
-            <h1>movie</h1>
-        </>
+        <Grid>
+            <Grid className="profile-table" style= {{borderSpacing: 0}}>
+                <tr>
+                    <div className='edit-profile-info-text'>Movies:</div>
+                </tr> 
+            </Grid>
+            <Grid container direction={"row"} spacing={2}>
+                <Grid item>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="500"
+                            image={avengerImage}
+                            alt="avengers"
+                        />
+                        <CardHeader                    
+                            title="Avengers"
+                            subheader="2022"
+                        />
+                        <Button variant="outlined">Watched</Button>
+                    </Card>
+                </Grid>
+                <Grid item>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="500"
+                            image={djangoImage}
+                            alt="django"
+                        />
+                        <CardHeader                    
+                            title="Django Unchained"
+                            subheader="2012"
+                        />
+                        <Button variant="outlined">Watched</Button>
+                    </Card>
+                </Grid>
+                <Grid item>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="500"
+                            image={jackImage}
+                            alt="jack"
+                        />
+                        <CardHeader                    
+                            title="Jack the Giant Slayer"
+                            subheader="2013"
+                        />
+                        <Button variant="outlined">Watched</Button>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Grid>        
     );
 }
 
-export default movies;
+export default Movies;
