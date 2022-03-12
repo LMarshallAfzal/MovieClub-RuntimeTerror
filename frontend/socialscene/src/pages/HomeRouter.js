5e7d4b7f083429c3977abeda151f23905d263a45
kurehao01 <alessio.wu@kcl.ac.uk>
2022-03-12 23:08:37 +0000

    Complete other's profile page react.js

--- frontend/socialscene/src/pages/HomeRouter.js
+++ frontend/socialscene/src/pages/HomeRouter.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../styling/pages/HomeRouter.css";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import NameHeader from "../components/NameHeader";
-import {Grid} from "@mui/material";
+import { Grid } from "@mui/material";
 import Dashboard from "./Dashboard";
 import Clubs from "./Clubs";
 import Movies from "./Movies";
@@ -12,6 +12,7 @@ import Profile from "./Profile";
 import Options from "./Options";
 import Home from "./Home";
 import Login from "./Login";
+import OthersProfile from "./OthersProfile";
 import ChangePassword from "./ChangePassword";
 
 function HomeRouter() {
@@ -20,10 +21,10 @@ function HomeRouter() {
     const userLogin = (tok) => {
         setToken(tok);
     }
-    
+
     localStorage.setItem('token', JSON.stringify("Token " + token))
-    if(!token) {
-        return <Login userLogin={userLogin}/>
+    if (!token) {
+        return <Login userLogin={userLogin} />
     }
 
     return (
@@ -33,19 +34,20 @@ function HomeRouter() {
                     <NameHeader
                         firstName={"noah"}
                         lastName={"cheeseman"}
-                        joinDate={"2022"}/>
+                        joinDate={"2022"} />
                     <Sidebar />
                 </Grid>
                 <Grid className={"home-grid-R-content"} item xs={9} padding={2} >
                     <Routes>
-                        <Route path={"/"} element={(<Home />)}/>
-                        <Route path={"dashboard"} element={(<Dashboard />)}/>
-                        <Route path={"clubs"} element={(<Clubs />)}/>
-                        <Route path={"movies"} element={(<Movies />)}/>
-                        <Route path={"whats-on"} element={(<WhatsOn />)}/>
-                        <Route path={"profile"} element={(<Profile />)}/>
-                        <Route path={"options"} element={(<Options />)}/>
-                        <Route path={"change-password"} element={(<ChangePassword />)}/>
+                        <Route path={"/"} element={(<Home />)} />
+                        <Route path={"dashboard"} element={(<Dashboard />)} />
+                        <Route path={"clubs"} element={(<Clubs />)} />
+                        <Route path={"movies"} element={(<Movies />)} />
+                        <Route path={"whats-on"} element={(<WhatsOn />)} />
+                        <Route path={"profile"} element={(<Profile />)} />
+                        <Route path={"options"} element={(<Options />)} />
+                        <Route path={"change-password"} element={(<ChangePassword />)} />
+                        <Route path={"others-profile"} element={(<OthersProfile />)} />
                     </Routes>
                 </Grid>
             </Grid>
