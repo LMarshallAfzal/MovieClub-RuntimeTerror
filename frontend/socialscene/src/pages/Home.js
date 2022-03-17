7471c2df059fbb49ab7a820df136ce9d638564b1
kurehao01 <alessio.wu@kcl.ac.uk>
2022-03-17 18:41:45 +0000

    Make notifications for organisers to create meetings

--- frontend/socialscene/src/pages/Home.js
+++ frontend/socialscene/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import "../styling/pages/Home.css";
 import { Card, CardContent, Typography, CardActions, Button, Divider, List, Paper, ListSubheader, Grid, TextField, Stack, Box, ListItemButton, ListItemText, Autocomplete } from "@mui/material";
-import { Divider, List, Paper, ListSubheader, Grid, TextField, Stack, Box, ListItemButton, ListItemText, Autocomplete } from "@mui/material";
 import FormButton from "../components/FormButton";
 import { DummyDashboardClubsData, meetings, movies } from './DummyDashboardClubsData';
 import { useState } from 'react'
@@ -71,151 +70,43 @@ class Home extends React.Component {
                 component="span"
                 sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
             >
-                •
             </Box>
         );
         return (
             <>
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography variant="h5" component="div" paddingBottom={"10px"}>
-                            {/* Change 'this club' to club name. */}
-                            You are the organiser of *this club*. 
-                        </Typography>
-                        <Typography variant="h8">
-                            Create a new meeting for the club.
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <FormButton text="Create new meeting" size="small"></FormButton>
-                    </CardActions>
-                </Card>
+                <Grid className='' style={{ borderSpacing: 0 }}>
+                    <Grid
+                        container
+                        direction={"row"}
+                        justifyContent="space-evenly"
+                        alignItems="flex-start"
+                    >
+                        <Grid item xs={12}>
+                            <div className='dashboard-text'>Dashboard</div>
+                        </Grid>
+                        <Paper style={{ maxHeight: 300, width: '100%' }}>
+                            {/* {* notifications to organiser need to create meeting*.map((val) => { */}
+                            {/* return */}
+                            <Card sx={{ width: "100%" }}>
+                                <CardContent>
+                                    <Typography variant="h5" component="div" paddingBottom={"10px"}>
+                                        {/* Change 'this club' to club name. */}
+                                        You are the organiser of *this club*.
+                                    </Typography>
+                                    <Typography variant="h8">
+                                        Create a new meeting for the club.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <FormButton text="Create new meeting" size="small"></FormButton>
+                                </CardActions>
+                            </Card>
+                            {/* })} */}
+                        </Paper>
+                    </Grid>
+                </Grid>
             </>
-            // <Grid className='' style={{ borderSpacing: 0 }}>
-            //     <Grid 
-            //         container 
-            //         direction={"row"} 
-            //         justifyContent="space-evenly"
-            //         alignItems="flex-start" 
-            //         spacing={2}
-            //     >
-            //         <Grid item xs={12}>
-            //             <div className='dashboard-text'>Dashboard</div>
-            //         </Grid>
-            //         <Grid item xs={12}>
-            //             <Autocomplete
-            //                 freeSolo
-            //                 id="search"
-            //                 disableClearable
-            //                 options={DummyDashboardClubsData.map((movie) => movie.name)}
-            //                 renderInput={(params) => (
-            //                     <TextField
-            //                         {...params}
-            //                         label="🔎︎ Search Clubs"
-            //                         InputProps={{
-            //                             ...params.InputProps,
-            //                             type: 'search',
-            //                         }}
-            //                     />
-            //                 )}
-            //             />
-            //         </Grid>
-            //         <Grid item xs={4}>
-            //             <div style={{ paddingBottom: '20px' }} className='list-header-text'>
-            //                 My Clubs
-            //             </div>
-            //             <Paper style={{ maxHeight: 300, overflow: 'auto' }}>
-            //                 {this.state.myClubData.map((val) => {
-            //                     return (
-            //                         <ListItemButton>
-            //                             <ListItemText primary={val.club_name} />
-            //                         </ListItemButton>
-            //                     );
-            //                 })}
-            //             </Paper>
-            //         </Grid>
-            //         <Grid item xs={4}>
-            //             <div style={{ paddingBottom: '20px' }} className='list-header-text'>Clubs for You</div>
-            //             <Paper style={{ maxHeight: 300, overflow: 'auto' }}>
-            //                 {DummyDashboardClubsData.map((val) => {
-            //                     return <ListItemButton>
-            //                         <ListItemText primary={val.name} />
-            //                     </ListItemButton>
-            //                 })}
-            //             </Paper>
-            //         </Grid>
-            //         <Grid item xs={4}>
-            //             <Stack spacing={3}>
-            //                 <div className='list-header-text'>Create new club:</div>
-            //                 <TextField 
-            //                     className='dashboard-text-box' 
-            //                     id="outlined-basic" 
-            //                     label="Name" 
-            //                     name="club_name"
-            //                     variant="outlined"
-            //                     onChange={this.changeHandler}
-            //                     value={this.state.club_name} 
-            //                 />
-            //                 <TextField 
-            //                     className='dashboard-text-box' 
-            //                     id="outlined-basic" 
-            //                     label="Description" 
-            //                     name="mission_statement"
-            //                     variant="outlined" 
-            //                     onChange={this.changeHandler}
-            //                     value={this.state.mission_statement} 
-            //                 />
-            //                 <TextField 
-            //                     className='dashboard-text-box' 
-            //                     id="outlined-basic" 
-            //                     label="Themes" 
-            //                     name="themes"
-            //                     variant="outlined"
-            //                     onChange={this.changeHandler}
-            //                     value={this.state.themes} 
-            //                 />
-            //                 <div className={"single-button"}>
-            //                     <Box
-            //                         sx={{
-            //                             display: 'grid',
-            //                             gridAutoColumns: '1fr',
-            //                             gap: 1,
-            //                         }}
-            //                     >
-            //                         <Box sx={{ gridRow: '1', gridColumn: 'span 1' }}>
-            //                             <FormButton
-            //                                 text={"Create"}
-            //                                 onClick={this.submitForm}
-            //                                 onChange={this.fetchMembershipData()}
-            //                             />
-            //                         </Box>
-            //                     </Box>
-            //                 </div>
-            //             </Stack>
-            //         </Grid>
-            //         <Grid item xs={8}>
-            //             <div style={{ paddingBottom: '20px' }} className='list-header-text'>Meetings</div>
-            //             <Paper style={{ maxHeight: 300, overflow: 'auto' }}>
-            //                 {meetings.map((val, key) => {
-            //                     return <ListItemButton>
-            //                         <ListItemText primary={`${val.name} | ${val.time} `} />
-            //                     </ListItemButton>
-            //                 })}
-            //             </Paper>
-            //         </Grid>
-            //         <Grid item xs={4}>
-            //             <div style={{ paddingBottom: '20px' }} className='list-header-text'>Watched Movies</div>
-            //             <Paper style={{ maxHeight: 300, overflow: 'auto' }}>
-            //                 {movies.map((val, key) => {
-            //                     return <ListItemButton>
-            //                         <ListItemText primary={`${val.name} | ${val.rating} `} />
-            //                     </ListItemButton>
-            //                 })}
-            //             </Paper>
-            //         </Grid>
-            //     </Grid>
-            // </Grid>
-        )
+        );
     }
 }
 
