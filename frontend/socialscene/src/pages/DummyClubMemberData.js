7001002a222ed1a17aacc05eb900211056ab96cc
noahcheeseman <noah.cheeseman@kcl.ac.uk>
2022-03-18 20:27:17 +0000

    finish club edit options (styling needed)

---
+++ frontend/socialscene/src/pages/DummyClubMemberData.js
@@ -0,0 +1,27 @@
+import React from "react";
+import icon1 from "../styling/example icons/icon1.jpeg";
+import icon2 from "../styling/example icons/icon2.jpeg";
+import icon3 from "../styling/example icons/icon3.jpeg";
+import icon4 from "../styling/example icons/icon4.jpeg";
+import icon5 from "../styling/example icons/icon5.jpeg";
+
+
+export const DummyClubMemberData = [
+    {firstName: "Noah", lastName: "Cheeseman", iconImage: icon1},
+    {firstName: "Leonard", lastName: "Marshall", iconImage: icon2},
+    {firstName: "Alessio", lastName: "Wu", iconImage: icon3},
+    {firstName: "Jose", lastName: "Alfrez", iconImage: icon4},
+    {firstName: "Zsolt", lastName: "Balla", iconImage: icon5},
+    {firstName: "Leonard", lastName: "Marshall", iconImage: icon2},
+    {firstName: "Alessio", lastName: "Wu", iconImage: icon3},
+    {firstName: "Jose", lastName: "Alfrez", iconImage: icon4},
+    {firstName: "Zsolt", lastName: "Balla", iconImage: icon5},
+    {firstName: "Leonard", lastName: "Marshall", iconImage: icon2},
+    {firstName: "Alessio", lastName: "Wu", iconImage: icon3},
+    {firstName: "Jose", lastName: "Alfrez", iconImage: icon4},
+    {firstName: "Zsolt", lastName: "Balla", iconImage: icon5},
+    {firstName: "Leonard", lastName: "Marshall", iconImage: icon2},
+    {firstName: "Alessio", lastName: "Wu", iconImage: icon3},
+    {firstName: "Jose", lastName: "Alfrez", iconImage: icon4},
+    {firstName: "Zsolt", lastName: "Balla", iconImage: icon5},
+]
