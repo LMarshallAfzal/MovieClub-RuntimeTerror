14ae8bc03fac7f2b905ed0a381f25e24a0b95b28
kurehao01 <alessio.wu@kcl.ac.uk>
2022-03-18 16:36:04 +0000

    Add movies from club, movies recommended and search bar in movie page

--- frontend/socialscene/src/pages/Movies.js
+++ frontend/socialscene/src/pages/Movies.js
@@ -1,22 +1,23 @@
-import React, {useState, useContext, useEffect} from "react";
-import { Grid, Button, Typography, Rating } from "@mui/material";
+import React, { useState, useContext, useEffect } from "react";
+import { TextField, Autocomplete, Grid, Button, Typography, Rating } from "@mui/material";
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import "../styling/pages/Movie.css";
 import moviePoster from '../styling/empty_movie_poster.png'
 import AuthContext from "../components/AuthContext";
+import { moviesWithPoster } from "./DummyMoviesData";
 
 
 const Movies = () => {
     const [movie, setMovie] = useState('')
     const [recommendedMovies, setRecommendedMovies] = useState([])
-    let {user, authTokens} = useContext(AuthContext)
+    let { user, authTokens } = useContext(AuthContext)
 
     useEffect(() => {
         getRecommendedMovies()
-    },[])
-    
+    }, [])
+
 
     let getRecommendedMovies = async (e) => {
         let response = await fetch('http://127.0.0.1:8000/rec/', {
@@ -57,13 +58,33 @@ const Movies = () => {
     // }
     return (
         <Grid>
-            <Grid className="profile-table" style={{ borderSpacing: 0 }}>
+            <Grid paddingBottom={"10px"} className="profile-table" style={{ borderSpacing: 0 }}>
                 <tr>
-                    <div className='edit-profile-info-text'>Movies</div>
+                    <div className='page-title'>Movies</div>
                 </tr>
             </Grid>
-            <Grid container direction={"row"} spacing={2}>
-                {recommendedMovies.map((movie) => {
+            <Autocomplete
+                        freeSolo
+                        id="search"
+                        disableClearable
+                        options={moviesWithPoster.map((movie) => movie.title)}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="🔎︎ Search Movies"
+                                InputProps={{
+                                    ...params.InputProps,
+                                    type: 'search',
+                                }}
+                            />
+                        )}
+                        
+                    />
+            {/* MOVIES FROM CLUBS TO WATCH */}
+            <Grid paddingTop={"20px"} container direction={"row"} spacing={2}>
+                <Grid item xs={12}><div className='page-subtitle'>Movies from clubs</div></Grid>
+                {/*SUBSTITUTE BELOW moviesWithPoster TO MOVIES FROM CLUBS TO WATCH*/}
+                {moviesWithPoster.map((movie) => {
                     return <Grid item>
                         <Card sx={{ maxWidth: 330 }}>
                             <CardMedia
@@ -75,21 +96,56 @@ const Movies = () => {
                             <CardHeader
                                 title={movie.title}
                             />
-                            <Typography component="legend">Rate {movie.title}</Typography>
+                            <div style={{ paddingLeft: "18px", paddingBottom: "10px" }}>
+                                <Typography component="legend">Rate {movie.title}</Typography>
                                 <Rating
-                                name="simple-controlled"
-                                precision={0.5}
-                                max={5}
+                                    name="simple-controlled"
+                                    precision={0.5}
+                                    max={5}
                                 // onChange={(event, newValue) => (this.setState({score: newValue, onChange: this.fetchAddRating(movie.id)}))}                                    
                                 />
+                                <Typography paddingTop={"10px"} component="legend"> <b>Watch before *Meeting date and time*</b></Typography>
+                            </div>
                             <Button sx={{ height: 38, width: 330 }} className="watched-button" variant="outlined">Watched?</Button>
                         </Card>
                     </Grid>
                 })}
             </Grid>
+            {/* MOVIES RECOMMENDED FROM AI TO USER*/}
+            <Grid paddingTop={"20px"} container direction={"row"} spacing={2}>
+                <Grid item xs={12}><div className='page-subtitle'>Movies recommended for you</div></Grid>
+                {/* SUBSTITUTE BELOW moviesWithPoster TO MOVIES RECOMMENDED FROM AI*/}
+                {moviesWithPoster.map((movie) => {
+                    return <Grid item>
+                        <Card sx={{ maxWidth: 330 }}>
+                            <CardMedia
+                                component="img"
+                                height="500"
+                                image={moviePoster}
+                                alt={movie.title}
+                            />
+                            <CardHeader
+                                title={movie.title}
+                            />
+                            <div style={{ paddingLeft: "18px", paddingBottom: "10px" }}>
+                                <Typography component="legend">Rate {movie.title}</Typography>
+                                <Rating
+                                    name="simple-controlled"
+                                    precision={0.5}
+                                    max={5}
+                                // onChange={(event, newValue) => (this.setState({score: newValue, onChange: this.fetchAddRating(movie.id)}))}                                    
+                                />
+                            </div>
+                            <Button sx={{ height: 38, width: 330 }} className="watched-button" variant="outlined">Watched?</Button>
+                        </Card>
+                    </Grid>
+                })}
+                {/* SEARCH FOR MOVIES AND MARK AS WATCHED AND RATE IT */}
+
+            </Grid>
         </Grid>
     );
-    
+
 }
 
 export default Movies;
