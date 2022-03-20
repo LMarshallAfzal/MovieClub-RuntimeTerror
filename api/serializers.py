de843a6ce9deb1a722dbb2922404a9a9c7322e56
josefsousa09 <jfrancisco.mail@gmail.com>
2022-03-20 05:32:05 +0000

    Rename themes to theme to pass all tests.

--- api/serializers.py
+++ api/serializers.py
@@ -204,8 +204,8 @@ class CreateClubSerializer(serializers.Serializer):
         validators=[MaxLengthValidator(500)]
     )
 
-    themes = serializers.CharField(
-        required=False,
+    theme = serializers.CharField(
+        required=True,
         validators=[MaxLengthValidator(500)]
     )
 
@@ -217,7 +217,7 @@ class CreateClubSerializer(serializers.Serializer):
         club = Club.objects.create(
             club_name=validated_data['club_name'],
             mission_statement=validated_data['mission_statement'],
-            themes=validated_data['themes'],
+            theme=validated_data['theme'],
         )
         club.save()
 
