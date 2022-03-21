051f45b66544d16e8ceac6605f528f0a4ecdde03
noahcheeseman <noah.cheeseman@kcl.ac.uk>
2022-03-21 12:55:49 +0000

    update Login.js formatting again

--- frontend/socialscene/src/pages/home/Profile.js
+++ frontend/socialscene/src/pages/home/Profile.js
@@ -117,132 +117,121 @@ const Profile = () => {
     },[])
 
     return (
-        <table className='profile-table' >
-            <tr>
-                <div className='edit-profile-info-text'>Edit profile info</div>
-            </tr>
-                <Grid item>
-                    <Box component="form" onSubmit={submitChangeProfileForm}>
-                        <Stack spacing={2}>
-                            <TextField className='profile-text-box'
-                                error={usernameError}
-                                helperText={errorUsernameText}
-                                required
-                                id={"outlined"}
-                                label={"username"}
-                                name={"username"}
-                                type={"text"}
-                                variant={"outlined"}
-                                value={userData.username}
-                                onChange={e => onChange(e)}
-                            />
-                            <TextField className='profile-text-box'
-                                error={firstNameError}
-                                helperText={errorFirstNameText}
-                                required
-                                id={"outlined-basic"}
-                                label={"first name"}
-                                name={"first_name"}
-                                type={"text"}
-                                variant={"outlined"}
-                                value={userData.first_name}
-                                onChange={e => onChange(e)}
-                            />
-                            <TextField className='profile-text-box'
-                                error={lastNameError}
-                                helperText={errorLastNameText}
-                                required
-                                id={"outlined-basic"}
-                                label={"last name"}
-                                name={"last_name"}
-                                type={"text"}
-                                variant={"outlined"}
-                                value={userData.last_name}
-                                onChange={e => onChange(e)}
-                            />
-                            <TextField className='profile-text-box'
-                                error={emailError}
-                                helperText={errorEmailText}
-                                required
-                                id={"outlined-basic"}
-                                label={"email"}
-                                name={"email"}
-                                type={"email"}
-                                variant={"outlined"}
-                                value={userData.email}
-                                onChange={e => onChange(e)}
-                            />
-                            <TextField className='profile-text-box'
-                                error={bioError}
-                                helperText={errorBioText}
-                                spacing={6}
-                                id={"outlined-multiline-static"}
-                                label={"bio"}
-                                name={"bio"}
-                                type={"text"}
-                                variant={"outlined"}
-                                multiline
-                                rows={7.5}
-                                value={userData.bio}
-                                onChange={e => onChange(e)}
-                            />
-                            <div className={"single-button"}>
-                                <Box
-                                    sx={{
-                                        display: 'grid',
-                                        gridAutoColumns: '1fr',
-                                        gap: 1,
-                                    }}
-                                >
-                                    <Box sx={{ gridRow: '1', gridColumn: 'span 1' }}>
-                                        <FormButton
-                                            text={"cancel"}
-                                        />
-                                    </Box>
-                                </Box>
-                            </div>
-                        </Stack>
-                    {/* </td> */}
-                    {/* <td className='text-field'> */}
-                        <Stack spacing={2}>
-                            <TextField className='profile-text-box'
-                                error={preferencesError}
-                                helperText={errorPreferencesText}
-                                required
-                                spacing={6}
-                                id={"outlined-multiline-static"}
-                                label={"preferences"}
-                                name={"preferences"}
-                                type={"text"}
-                                variant={"outlined"}
-                                multiline
-                                rows={20}
-                                value={userData.preferences}
-                                onChange={e => onChange(e)}
-                            />
-                            <div className={"single-button"}>
-                                <Box
-                                    sx={{
-                                        display: 'grid',
-                                        gridAutoColumns: '1fr',
-                                        gap: 1,
-                                    }}
-                                >
-                                    <Box sx={{ gridRow: '1', gridColumn: 'span 1' }}>
-                                        <FormButton
-                                            type="submit"
-                                            text={"submit"}   
-                                            onClick={submitChangeProfileForm}
-                                        />        
-                                    </Box>
-                                </Box>
-                            </div>
-                        </Stack>
-                    {/* </td> */}
-                
-                </Box>
-            </Grid>    
-        </table >
+       <Grid container
+              direction={"row"}
+              spacing={2}
+        >
+
+            <Grid item xs={12}>
+
+                <div className={"home-page-title"}>
+
+                    <h3>profile<h3--emphasise>.</h3--emphasise></h3>
+                </div>
+            </Grid>
+
+            <Grid item xs={12}>
+
+                <form onSubmit={submitChangeProfileForm}>
+
+                    <Stack spacing={2}>
+
+                        <TextField className={"profile-text-box"}
+                                   error={usernameError}
+                                   helperText={errorUsernameText}
+                                   required
+                                   id={"outlined"}
+                                   label={"username"}
+                                   name={"username"}
+                                   type={"text"}
+                                   variant={"outlined"}
+                                   value={userData.username}
+                                   onChange={e => onChange(e)}
+                        />
+
+                        <TextField className={"profile-text-box"}
+                                   error={firstNameError}
+                                   helperText={errorFirstNameText}
+                                   required
+                                   id={"outlined-basic"}
+                                   label={"first name"}
+                                   name={"first_name"}
+                                   type={"text"}
+                                   variant={"outlined"}
+                                   value={userData.first_name}
+                                   onChange={e => onChange(e)}
+                        />
+
+                        <TextField className={"profile-text-box"}
+                                   error={lastNameError}
+                                   helperText={errorLastNameText}
+                                   required
+                                   id={"outlined-basic"}
+                                   label={"last name"}
+                                   name={"last_name"}
+                                   type={"text"}
+                                   variant={"outlined"}
+                                   value={userData.last_name}
+                                   onChange={e => onChange(e)}
+                        />
+
+                        <TextField className={"profile-text-box"}
+                                   error={emailError}
+                                   helperText={errorEmailText}
+                                   required
+                                   id={"outlined-basic"}
+                                   label={"email"}
+                                   name={"email"}
+                                   type={"email"}
+                                   variant={"outlined"}
+                                   value={userData.email}
+                                   onChange={e => onChange(e)}
+                        />
+
+                        <TextField className={"profile-text-box"}
+                                   error={bioError}
+                                   helperText={errorBioText}
+                                   spacing={6}
+                                   id={"outlined-multiline-static"}
+                                   label={"bio"}
+                                   name={"bio"}
+                                   type={"text"}
+                                   variant={"outlined"}
+                                   multiline
+                                   rows={7.5}
+                                   value={userData.bio}
+                                   onChange={e => onChange(e)}
+                        />
+
+                        <FormButton
+                            text={"cancel"}
+                        />
+
+                        <TextField className={"profile-text-box"}
+                                   error={preferencesError}
+                                   helperText={errorPreferencesText}
+                                   required
+                                   spacing={6}
+                                   id={"outlined-multiline-static"}
+                                   label={"preferences"}
+                                   name={"preferences"}
+                                   type={"text"}
+                                   variant={"outlined"}
+                                   multiline
+                                   rows={20}
+                                   value={userData.preferences}
+                                   onChange={e => onChange(e)}
+                        />
+
+                        <FormButton
+                            text={"save"}
+                            type={"submit"}
+                        />
+                    </Stack>
+                </form>
+            </Grid>
+       </Grid>
     );
 }
+
 export default Profile;
