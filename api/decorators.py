0615834ee9f09c64e379790a06bc7d552a49f7d1
josefsousa09 <jfrancisco.mail@gmail.com>
2022-03-23 04:31:58 +0000

    Fix has_ratings decorators to pass tests.

--- api/decorators.py
+++ api/decorators.py
@@ -1,5 +1,5 @@
 from .models import Movie, Club, Rating, Watch, Membership
-from .helpers import get_initial_recommendations_for_clubs,get_initial_recommendations_for_movies
+from .helpers import get_initial_recommendations_for_clubs, get_initial_recommendations_for_movies
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework import status
@@ -83,18 +83,79 @@ def is_organiser(view_function):
             return view_function(request, club_id, *args, **kwargs)
     return modified_view_function
 
+
+def is_owner(view_function):
+    @wraps(view_function)
+    def modified_view_function(request, club_id, *args, **kwargs):
+        club = Club.objects.get(id=club_id)
+        try:
+            Membership.objects.get(user=request.user, club=club, role="C")
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        else:
+            return view_function(request, club_id, *args, **kwargs)
+    return modified_view_function
+
+
+def not_banned(view_function):
+    @wraps(view_function)
+    def modified_view_function(request, club_id, *args, **kwargs):
+        club = Club.objects.get(id=club_id)
+        try:
+            Membership.objects.get(user=request.user, club=club, role="B")
+        except ObjectDoesNotExist:
+            return view_function(request, club_id, *args, **kwargs)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+    return modified_view_function
+
+
+def is_banned(view_function):
+    @wraps(view_function)
+    def modified_view_function(request, club_id, user_id, *args, **kwargs):
+        club = Club.objects.get(id=club_id)
+        try:
+            user = User.objects.get(id=user_id)
+            Membership.objects.get(user=user, club=club, role="B")
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        else:
+            return view_function(request, club_id,user_id, *args, **kwargs)
+
+    return modified_view_function
+
+
+def user_in_club(view_function):
+    def modified_view_function(request, club_id, *args, **kwargs):
+        user = request.user
+        club = Club.objects.get(id=club_id)
+        try:
+            for arg in kwargs.values():
+                user = User.objects.get(id=arg)
+            role = club.get_club_membership(user)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        else:
+            return view_function(request, club_id, *args, **kwargs)
+    return modified_view_function
+
+
 def has_ratings_for_movie_recommendations(view_function):
     @wraps(view_function)
     def modified_view_function(request, *args, **kwargs):
         try:
             Rating.objects.all().filter(user=request.user.id)
         except ObjectDoesNotExist:
-            recommendations = get_initial_recommendations_for_movies(request.user,request.user.get_user_preferences().split(','))
+            recommendations = get_initial_recommendations_for_movies(
+                request.user, request.user.get_user_preferences().split(','))
             serializer = MovieSerializer(recommendations, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return view_function(request, *args, **kwargs)
     return modified_view_function
+
 
 def has_ratings_for_club_recommendations(view_function):
     @wraps(view_function)
@@ -102,10 +163,10 @@ def has_ratings_for_club_recommendations(view_function):
         try:
             Rating.objects.all().filter(user=request.user.id)
         except ObjectDoesNotExist:
-            recommendations = get_initial_recommendations_for_clubs(request.user,request.user.get_user_preferences().split(','))
+            recommendations = get_initial_recommendations_for_clubs(
+                request.user, request.user.get_user_preferences().split(','))
             serializer = ClubSerializer(recommendations, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return view_function(request, *args, **kwargs)
     return modified_view_function
-
