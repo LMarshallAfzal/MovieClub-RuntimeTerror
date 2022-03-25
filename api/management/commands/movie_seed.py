18960d51f44e205aec63454b72eac1c1896f5bfe
Zsolt Balla <balla.zsolt.krisztian@gmail.com>
2022-03-25 11:43:40 +0000

    Change movie seeder to create & link Genre objects

--- api/management/commands/movie_seed.py
+++ api/management/commands/movie_seed.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand
-from api.models import Movie
+from api.models import Genre, Movie
 from django.db import IntegrityError
 import pandas as pd
 
+def get_genre_id(genre_name):
+    try:
+        return Genre.objects.get(name=genre_name).id
+    except Genre.DoesNotExist:
+        new_genre = Genre.objects.create(name=genre_name)
+        return new_genre.id
 
 class Command(BaseCommand):
     
@@ -14,11 +20,14 @@ class Command(BaseCommand):
         movie_count = 0
         for index,row in file.iterrows():
             print(f'Seeding movie {movie_count}',  end='\r')
-            Movie.objects.create(
+
+            genres = [get_genre_id(genre) for genre in row['genres'].split('|')]
+            
+            movie = Movie.objects.create(
                 ml_id = int(row['movieId']),
                 title = row['title'],
-                genres = row['genres'].replace('|',','),
                 year = int(row['year'])
             )
+            movie.genres.set(genres)
             movie_count+=1
         print('Movie seeding complete')
