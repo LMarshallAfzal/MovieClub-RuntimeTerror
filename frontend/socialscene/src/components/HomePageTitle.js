d3ef1cf9d3b777459891536495e7c6e4f73371e5
noahcheeseman <noah.cheeseman@kcl.ac.uk>
2022-03-30 21:37:01 +0100

    adjust z value

--- frontend/socialscene/src/components/HomePageTitle.js
+++ frontend/socialscene/src/components/HomePageTitle.js
@@ -4,7 +4,7 @@ import "../styling/pages/HomePageTitle.css";
 
 function HomePageTitle(props) {
     return (
-        <Grid item xs={12} sx={{ boxShadow: 3, position: "sticky", top: "0px", zIndex: 90000}}>
+        <Grid item xs={12} sx={{ boxShadow: 3, position: "sticky", top: "0px", zIndex: 1000}}>
 
             <div className={"home-page-title"}>
                 <h3>{props.title}<h3--emphasise>.</h3--emphasise></h3>
