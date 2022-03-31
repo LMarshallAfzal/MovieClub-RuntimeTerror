b6e6fbd7ba26572a3b1876bef3a08462651f312d
Binh <13987638+koblihh@users.noreply.github.com>
2022-03-31 19:45:38 +0100

    Re-write tests for sign up component
    - Possibly re-write skipped tests

--- frontend/socialscene/src/__tests__/Signup.test.js
+++ frontend/socialscene/src/__tests__/Signup.test.js
@@ -1,50 +1,128 @@
-/* eslint-disable testing-library/prefer-screen-queries */
-import React, {useContext} from 'react';
-import {render, fireEvent, screen, waitFor} from "@testing-library/react";
+import React from "react";
 import "@testing-library/jest-dom";
-import Signup from "../pages/root/Signup";
+import userEvent from "@testing-library/user-event";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+
+import Signup from "../pages/core/Signup";
 import AuthContext from "../components/helper/AuthContext";
-import {MemoryRouter} from 'react-router-dom'
-import user from '@testing-library/user-event';
-// import { check } from 'prettier';
 
+// This mocks the
+// <CsrfToken /> component
+jest.mock("../components/helper/CsrfToken", () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return <></>;
+    },
+  };
+});
 
+// This mocks the
+// useNavigate function
+// Why "mock*" must be appended?
+const mockNavigate = jest.fn();
+// This code snippet must
+// be placed outside of describe(...)
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
-function renderAuthContext(authTokens) {
-  return render (
-    <AuthContext.Provider value={{authTokens}}>
-      <MemoryRouter>
-        <Signup />
-      </MemoryRouter>
-    </AuthContext.Provider> 
-  )
-}
+describe("Sign up form", () => {
+  let fetchMock;
 
-describe('Signup', () => {
-  let authTokens;
   beforeEach(() => {
-    authTokens = {
-      accessToken: 'accessToken',
-      refreshToken: 'refreshToken'
-    }
-  }
-  )
-  it('should render the signup page', () => {
-    const {getByText} = renderAuthContext(authTokens);
-    expect(getByText('sign up')).toBeInTheDocument();
+    // This mocks the
+    // fetch function
+    fetchMock = jest
+      .spyOn(window, "fetch")
+      .mockImplementation(() =>
+        Promise.resolve({ json: () => Promise.resolve([]) })
+      );
+    // This loads the
+    // sign up screen
+    render(
+      <AuthContext.Provider
+        value={{
+          loginUser: jest.fn(),
+          logoutUser: jest.fn(),
+        }}
+      >
+        <Signup />
+      </AuthContext.Provider>
+    );
+  });
 
-  })
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
 
-  it('should render the signup form', () => {
-    const {getByTestId} = renderAuthContext(authTokens);
-    expect(getByTestId('username-field')).toBeInTheDocument();
-    expect(getByTestId('first-name-field')).toBeInTheDocument();
-    expect(getByTestId('last-name-field')).toBeInTheDocument();
-    expect(getByTestId('email-field')).toBeInTheDocument();
-    expect(getByTestId('bio-field')).toBeInTheDocument();
-    expect(getByTestId('preferences-field')).toBeInTheDocument();
-    expect(getByTestId('password-field')).toBeInTheDocument();
-    expect(getByTestId('password-confirmation-field')).toBeInTheDocument();
-  })
+  test("Username field exists", () => {
+    expect(
+      screen.getByRole("textbox", { name: /username/i })
+    ).toBeInTheDocument();
+  });
 
-})
+  test("Username input is changed", () => {
+    const usernameInput = screen.getByLabelText(/username/i);
+    expect(usernameInput).toHaveValue(""); // before
+    fireEvent.change(usernameInput, { target: { value: "@johndoe" } });
+    expect(usernameInput).toHaveValue("@johndoe"); // after
+  });
+
+  // Do we care about HTML form validation
+  // to ensure that the entered data
+  // is in the proper format?
+
+  test.skip("Given invalid input data when sign up form is submitted then error message is shown", () => {
+    // Empty input...
+
+    const submitButton = screen.getByRole("button", { name: /sign.up/i });
+    userEvent.click(submitButton);
+
+    expect(fetchMock).toHaveBeenCalled();
+    expect(fetchMock).toHaveBeenCalledWith(
+      "http://127.0.0.1:8000/sign_up/",
+      expect.any(Object)
+    );
+    waitFor(() => expect(screen.findByText(/error/i)).toBeInTheDocument());
+  });
+
+  test.skip("Given valid input data when sign up form is submitted then no error message is shown", () => {
+    fireEvent.change(screen.getByLabelText(/username/i), {
+      target: { value: "@janedoe" },
+    });
+    fireEvent.change(screen.getByLabelText(/first.name/i), {
+      target: { value: "Jane" },
+    });
+    fireEvent.change(screen.getByLabelText(/last.name/i), {
+      target: { value: "Doe" },
+    });
+    fireEvent.change(screen.getByLabelText(/email/i), {
+      target: { value: "janedoe@example.org" },
+    });
+    fireEvent.change(screen.getByLabelText(/bio/i), {
+      target: { value: "The quick brown fox jumps over the lazy dog." },
+    });
+    fireEvent.change(screen.getByLabelText(/preferences/i), {
+      target: { value: "Romance" },
+    });
+    fireEvent.change(screen.getByLabelText(/password/i), {
+      target: { value: "Password123" },
+    });
+    fireEvent.change(screen.getByLabelText(/password.confirm/i), {
+      target: { value: "Password123" },
+    });
+
+    const submitButton = screen.getByRole("button", { name: /sign.up/i });
+    userEvent.click(submitButton);
+
+    expect(fetchMock).toHaveBeenCalled();
+    expect(fetchMock).toHaveBeenCalledWith(
+      "http://127.0.0.1:8000/sign_up/",
+      expect.any(Object)
+    );
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
+  });
+});
