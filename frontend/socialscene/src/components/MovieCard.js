7c9af6bde2bc818501e53402ad0869e46f0195dd
josefsousa09 <jfrancisco.mail@gmail.com>
2022-04-05 09:37:03 +0100

    Redirect for movie detail.
    Co-authored-by: Binh <13987638+koblihh@users.noreply.github.com>

--- frontend/socialscene/src/components/MovieCard.js
+++ frontend/socialscene/src/components/MovieCard.js
@@ -1,174 +1,180 @@
-import React, {useEffect, useState} from "react";
-import {Card, CardMedia, Chip, Grid, Rating, Stack, Tooltip} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Chip,
+  Grid,
+  Rating,
+  Stack,
+  Tooltip,
+} from "@mui/material";
 import "../styling/components/MovieCard.css";
 import ThemeButton from "./core/ThemeButton";
 import MovieWatchRateDialog from "./helper/MovieWatchRateDialog";
 import LoadingSkeleton from "./helper/LoadingSkeleton";
 import placeholder from "../resources/images/empty_movie_poster.png";
-import {useNavigate} from "react-router";
-import axios from "axios"
+import { useNavigate } from "react-router";
+import axios from "axios";
 
 function MovieCard(props) {
-    const [watchedMovies, setWatchedMovies] = useState([]);
-    const [showPrompt, setShowPrompt] = useState(false);
-    const [promptData, setPromptData] = useState("");
-    const [cardWidth, setCardWidth] = useState(150);
-    const [cardBorder, setCardBorder] = useState("0px solid black");
-    const [loaded, setLoaded] = useState(true);
-    const [movie, setMovie] = useState('');
+  const [watchedMovies, setWatchedMovies] = useState([]);
+  const [showPrompt, setShowPrompt] = useState(false);
+  const [promptData, setPromptData] = useState("");
+  const [cardWidth, setCardWidth] = useState(150);
+  const [cardBorder, setCardBorder] = useState("0px solid black");
+  const [loaded, setLoaded] = useState(true);
+  const [movie, setMovie] = useState("");
 
-    useEffect(() => {
-        getImage();
-    }, []);
+  useEffect(() => {
+    getImage();
+  }, []);
 
-    const url = 'http://www.omdbapi.com/?i=tt'+props.movie.imdb_id+'&apikey=d938f360'
+  const url =
+    "http://www.omdbapi.com/?i=tt" + props.movie.imdb_id + "&apikey=d938f360";
 
-    const getImage = () => {
-        axios.get(`${url}`)
-        .then ((response) => {
-            const image = response.data.Poster
-            setMovie(image)
-        })
-    }
+  const getImage = () => {
+    axios.get(`${url}`).then((response) => {
+      const image = response.data.Poster;
+      setMovie(image);
+    });
+  };
 
-    let moviePoster = movie;
+  let moviePoster = movie;
 
-    const closePrompt = () => {
-        setShowPrompt(false);
-    }
+  const closePrompt = () => {
+    setShowPrompt(false);
+  };
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const HandleNavigate = (location) => {
-        return (
-            navigate(`${location}`, {replace: false})
-        )
-    }
+  const HandleNavigate = (location) => {
+    return navigate(`${location}`, { replace: false });
+  };
 
-    //MovieCard is passed the movie object, all movie interactions occurs here
+  //MovieCard is passed the movie object, all movie interactions occurs here
 
-    function ClubMovie() {
-        if (props.clubMovie === true) {
-            const toolText = `event: {event.title} 
+  function ClubMovie() {
+    if (props.clubMovie === true) {
+      const toolText = `event: {event.title} 
                             ${props.movie.deadline}
                             at {eventtime}`;
-            return (
-                <>
-                    <Tooltip
-                        arrow
-                        placement={"right"}
-                        title={
-                            <>
-                                <p className={"movie-card-event"}>event</p>
-                                <h6>{props.movie.deadline}</h6>
-                                <h6>17:30</h6>
-                            </>
-                        }>
-                        <Chip
-                            label={props.movie.club}
-                            onClick={() => HandleNavigate("/home/discussion")}
-                        />
-                    </Tooltip>
-                </>
-            )
-        } else {
-            return (
-                <></>
-            )
-        }
+      return (
+        <>
+          <Tooltip
+            arrow
+            placement={"right"}
+            title={
+              <>
+                <p className={"movie-card-event"}>event</p>
+                <h6>{props.movie.deadline}</h6>
+                <h6>17:30</h6>
+              </>
+            }
+          >
+            <Chip
+              label={props.movie.club}
+              onClick={() => HandleNavigate("/home/discussion")}
+            />
+          </Tooltip>
+        </>
+      );
+    } else {
+      return <></>;
     }
+  }
 
-    function RateMovie() {
-        if (props.rateMovie === true) {
-            return (
-                <>
-                    <MovieWatchRateDialog movie={props.movie} isOpen={showPrompt} onClose={closePrompt} data={promptData}/>
-                    <ThemeButton
-                        text={"watch"}
-                        style={"primary"}
-                        onClick={() => {
-                            setPromptData(props.movie);
-                            setShowPrompt(true);
-                        }}
-                    />
-                </>
-            )
-        } else {
-            return (
-                <></>
-            )
-        }
+  function RateMovie() {
+    if (props.rateMovie === true) {
+      return (
+        <>
+          <MovieWatchRateDialog
+            movie={props.movie}
+            isOpen={showPrompt}
+            onClose={closePrompt}
+            data={promptData}
+          />
+          <ThemeButton
+            text={"watch"}
+            style={"primary"}
+            onClick={() => {
+              setPromptData(props.movie);
+              setShowPrompt(true);
+            }}
+          />
+        </>
+      );
+    } else {
+      return <></>;
     }
+  }
 
-    return (
-        <Grid item>
-            <LoadingSkeleton loading={loaded}>
-                <Card sx={{
-                    width: cardWidth,
-                    transition: "ease-out",
-                    transitionDuration: "0.2s",
-                    border: cardBorder
-                }}
-                      onMouseEnter={() => {
-                          setCardWidth(160)
-                          setCardBorder("2px solid red")
-                      }}
-                      onMouseLeave={() => {
-                          setCardWidth(150)
-                          setCardBorder("0px solid black")
-                      }}
-                      translate={"yes"}
-                >
+  return (
+    <Grid item>
+      <LoadingSkeleton loading={loaded}>
+        <Card
+          sx={{
+            width: cardWidth,
+            transition: "ease-out",
+            transitionDuration: "0.2s",
+            border: cardBorder,
+          }}
+          onMouseEnter={() => {
+            setCardWidth(160);
+            setCardBorder("2px solid red");
+          }}
+          onMouseLeave={() => {
+            setCardWidth(150);
+            setCardBorder("0px solid black");
+          }}
+          translate={"yes"}
+        >
+          <CardActionArea onClick={() => HandleNavigate(props.movie.id)}>
+            <CardMedia
+              component={"img"}
+              sx={{ height: "100%" }}
+              image={moviePoster}
+              alt={props.movie.title}
+            />
 
+            <Tooltip
+              arrow
+              placement="right"
+              title={
+                <p className={"movie-card-event"}>{props.movie.rating} stars</p>
+              }
+            >
+              <Stack paddingTop={1} alignItems={"center"}>
+                <Rating
+                  readOnly
+                  sx={{ fontSize: "1.2em" }}
+                  precision={0.5}
+                  name={"read-only"}
+                  value={props.movie.rating}
+                />
+              </Stack>
+            </Tooltip>
 
-                    <CardMedia
-                        component={"img"}
-                        sx={{height: "100%"}}
-                        image={moviePoster}
-                        alt={props.movie.title}
-                    />
+            <Stack spacing={1} padding={1} alignItems={"left"}>
+              <Tooltip
+                arrow
+                placement="right"
+                title={
+                  <p className={"movie-card-event"}>{props.movie.title}</p>
+                }
+              >
+                <h6 className={"movie-card-title"}>{props.movie.title}</h6>
+              </Tooltip>
 
-                    <Tooltip
-                        arrow
-                        placement="right"
-                        title={
-                            <p className={"movie-card-event"}>{props.movie.rating} stars</p>
-                        }
-                    >
-                        <Stack paddingTop={1} alignItems={"center"}>
+              <ClubMovie />
 
-                            <Rating
-                                readOnly
-                                sx={{fontSize: "1.2em"}}
-                                precision={0.5}
-                                name={"read-only"}
-                                value={props.movie.rating}
-                            />
-
-                        </Stack>
-                    </Tooltip>
-
-                    <Stack spacing={1} padding={1} alignItems={"left"}>
-                        <Tooltip
-                            arrow
-                            placement="right"
-                            title={
-                                <p className={"movie-card-event"}>{props.movie.title}</p>
-                            }
-                        >
-                            <h6 className={"movie-card-title"}>{props.movie.title}</h6>
-                        </Tooltip>
-
-                        <ClubMovie/>
-
-                        <RateMovie/>
-
-                    </Stack>
-                </Card>
-            </LoadingSkeleton>
-        </Grid>
-    );
+              <RateMovie />
+            </Stack>
+          </CardActionArea>
+        </Card>
+      </LoadingSkeleton>
+    </Grid>
+  );
 }
 
 export default MovieCard;
-
