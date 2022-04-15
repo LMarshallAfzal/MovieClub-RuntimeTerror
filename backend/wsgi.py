4f7b9a1f03dc5d43bae9dc07d1e4a981cd04a658
Leonard Marshall Afzal <loma256@gmail.com>
2022-04-15 09:58:05 +0100

    Edit build in package.json

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
