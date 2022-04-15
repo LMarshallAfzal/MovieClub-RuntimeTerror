13432b39c296c9ac0f370d196d25bd2ac0ddb74a
Leonard Marshall Afzal <loma256@gmail.com>
2022-04-15 18:47:34 +0100

    Remove useFetch from homepage

--- src/pages/core/Homepage.js
+++ src/pages/core/Homepage.js
@@ -1,68 +1,76 @@
-import React,{ useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../../styling/pages/Homepage.css";
 import useFetch from "../../components/helper/useFetch";
 
-
 window.addEventListener("DOMContentLoaded", () => {
-    const spotlight = document.querySelector('.spotlight');
-    let spotlightSize = 'transparent 160px, rgba(0, 0, 0, 0.60) 200px)';
+	const spotlight = document.querySelector(".spotlight");
+	let spotlightSize = "transparent 160px, rgba(0, 0, 0, 0.60) 200px)";
 
-    window.addEventListener('mousemove', e => updateSpotlight(e));
+	window.addEventListener("mousemove", (e) => updateSpotlight(e));
 
-    function updateSpotlight(e) {
-        spotlight.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${(e.pageY - 90) / (window.innerHeight - 90) * 100}%, ${spotlightSize}`;
-
-    }
+	function updateSpotlight(e) {
+		spotlight.style.backgroundImage = `radial-gradient(circle at ${
+			(e.pageX / window.innerWidth) * 100
+		}% ${
+			((e.pageY - 90) / (window.innerHeight - 90)) * 100
+		}%, ${spotlightSize}`;
+	}
 });
 
 const Homepage = () => {
-    let api = useFetch()
+	let api = useFetch();
 
-    const [randomMovie, setRandomMovie] = useState()
-    
-    useEffect(() => {
-        getRandomMovie()
+	const [randomMovie, setRandomMovie] = useState();
 
-    }, [randomMovie]);
+	useEffect(() => {
+		getRandomMovie();
+	}, [randomMovie]);
 
-    const getRandomMovie = async () => {
+	const getRandomMovie = async () => {
+		let response = await fetch(
+			`https://social-scene.herokuapp.com/random_movie/`,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		let data = await response.json();
+		if (response.status === 200) {
+			console.log(`random id: ${data.imdb_id}`);
+			setRandomMovie(data.imdb_id);
+		}
+	};
 
-        let { response, data } = await api(`/random_movie/`, "GET");
-        if (response.status === 200) {
-        console.log(`random id: ${data.imdb_id}`)
-        setRandomMovie(data.imdb_id)
-        }
-      
-        // return (data.imdb_id)
+	// console.log(`test ${randomMovie}`)
 
-        // console.log(randomMovie)
-    };
+	// const movieIMDB = "0972785";
+	// const movieIMDB = getRandomMovie();
+	// console.log(movieIMDB);
 
+	// console.log(`movie: ${movieAPIData}`)
 
-    // console.log(`test ${randomMovie}`)
-
-
-    // const movieIMDB = "0972785";
-    // const movieIMDB = getRandomMovie();
-    // console.log(movieIMDB);
-
-    // console.log(`movie: ${movieAPIData}`)
-
-    return (
-        <>
-            <div className={"spotlight"}/>
-            <div className={"welcome-text"}>
-                <h2>welcome
-                    <h2--emphasise>.</h2--emphasise>
-                </h2>
-            </div>
-            <img className={'cover-image'}
-                 src={"https://img.omdbapi.com/?i=tt" + randomMovie + "&h=600&apikey=199b93be"}
-                 alt="blurred movie poster"
-            />
-            <div className={"welcome-poster"}/>
-
-        </>
-    );
-}
+	return (
+		<>
+			<div className={"spotlight"} />
+			<div className={"welcome-text"}>
+				<h2>
+					welcome
+					<h2--emphasise>.</h2--emphasise>
+				</h2>
+			</div>
+			<img
+				className={"cover-image"}
+				src={
+					"https://img.omdbapi.com/?i=tt" +
+					randomMovie +
+					"&h=600&apikey=199b93be"
+				}
+				alt="blurred movie poster"
+			/>
+			<div className={"welcome-poster"} />
+		</>
+	);
+};
 export default Homepage;
