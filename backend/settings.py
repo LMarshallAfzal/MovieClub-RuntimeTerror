c8c139491912f399950b9daf8dc8fea5313cefb3
Leonard Marshall Afzal <loma256@gmail.com>
2022-04-15 10:38:39 +0100

    Edit whitenoise in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -169,7 +169,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
