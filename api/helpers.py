7ec2bed288d8647d7cac5c2617e13905157bf989
josefsousa09 <jfrancisco.mail@gmail.com>
2022-04-20 02:09:46 +0400

    stuff

--- api/helpers.py
+++ api/helpers.py
@@ -9,14 +9,14 @@ def get_random_movies_helper(num_movies):
     query = []
     for movie in Movie.objects.all():
         query.append(movie.id)
-    movies = random.sample(query,num_movies)
+    movies = random.sample(query, num_movies)
     output = []
     for randomId in query:
         output.append(Movie.objects.get(id=randomId))
 
-
     return output
-    
+
+
 def recommendations_based_on_preferences_for_user_movies(user, user_preferences):
     query = Movie.get_movies_by_genre(user_preferences)
     watched_movies = user.get_watched_movies()
@@ -30,6 +30,7 @@ def recommendations_based_on_preferences_for_user_movies(user, user_preferences)
         number_of_recomendations = len(movies)
     recommendations = random.sample(movies, number_of_recomendations)
     return recommendations
+
 
 def recommendations_based_on_theme_for_meeting_movies(club):
     watched_movies = set()
@@ -50,18 +51,18 @@ def recommendations_based_on_theme_for_meeting_movies(club):
 
 
 def recommendations_based_on_preferences_for_clubs(user, user_preferences):
-    querysets = Club.get_clubs_by_theme(user_preferences)
+    queryset = Club.get_clubs_by_theme(user_preferences)
     clubs = set()
-    for queryset in querysets:
-        for club in queryset:
-            if not user in club.club_members.all():
-                clubs.add(club)
-            continue
+    for club in queryset:
+        if not user in club.club_members.all():
+            clubs.add(club)
+        continue
     number_of_recomendations = 5
     if len(clubs) < 5:
         number_of_recomendations = len(clubs)
     recommendations = random.sample(clubs, number_of_recomendations)
     return recommendations
+
 
 def update_upcoming_meetings():
     meetings = Club.objects.all().filter(club_meetings__completed=False)
@@ -69,7 +70,3 @@ def update_upcoming_meetings():
         if meeting.date <= datetime.date.today() and meeting.end_time <= datetime.now().time():
             meeting.completed = True
             meeting.save()
-
-
-
-
